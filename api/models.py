7b85b7b89d2473b4e67178ac5fc070a8874369d9
gkim <kimkm0419@naver.com>
2022-07-12 15:54:56 +0900

    🔥#2 Refactor : Remove not using choices

--- api/models.py
+++ api/models.py
@@ -37,15 +37,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    LEVEL_CHOICE = [
-        ('Ruby', 'Ruby'),
-        ('Diamond', 'Diamond'),
-        ('Platinum', 'Platinum'),
-        ('Gold', 'Gold'),
-        ('Silver', 'Silver'),
-        ('Bronze', 'Bronze'),
-    ]
-
     nickname = models.CharField(max_length=20, unique=True)
     email = models.CharField(max_length=200, unique=True)
 
