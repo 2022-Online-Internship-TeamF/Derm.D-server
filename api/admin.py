1190077ebe40256c56e01240c123b51af463489d
gkim <kimkm0419@naver.com>
2022-07-12 15:26:29 +0900

    ✨#2 Feat : Add models to admin page

--- api/admin.py
+++ api/admin.py
@@ -2,3 +2,13 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(User)
+admin.site.register(Archive)
+
+admin.site.register(Condition)
+admin.site.register(ConditionMedia)
+
+admin.site.register(Question)
+admin.site.register(QuestionMedia)
+
+admin.site.register(Answer)
+admin.site.register(AnswerMedia)
