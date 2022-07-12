0d3a7b1fcb45ffab8bdaa016cdc715e524c7e796
gkim <kimkm0419@naver.com>
2022-07-12 14:45:31 +0900

    🐬#2 Feat : Add models for conditions, archives, and q&a

--- api/models.py
+++ api/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 
+# class Hosptial(models.Model):
+# 기능 추가 시 병원 모델 추가
+
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -57,3 +61,59 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return '[{}] {}'.format(self.doctor_flag, self.nickname)
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Condition(BaseModel):
+    kr_name = models.CharField(max_length=30, blank=True)
+    eng_name = models.CharField(max_length=30)
+    description = models.TextField(null=True, blank=True)
+
+
+class Archive(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='archive')
+    condition = models.ForeignKey(Condition, on_delete=models.CASCADE, related_name='archive')
+
+
+class Question(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='question')
+    condition = models.ForeignKey(Condition, on_delete=models.CASCADE, related_name='question')
+
+    content = models.TextField()
+
+
+class Answer(BaseModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answer')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answer')
+    # hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, related_name='answer')
+
+    content = models.TextField()
+
+
+class ConditionMedia(BaseModel):
+    condition = models.ForeignKey(Condition, on_delete=models.CASCADE, related_name='conditionMedia')
+
+    img = models.ImageField(blank=True)
+    main_flag = models.BooleanField(default=False)
+
+
+class QuestionMedia(BaseModel):
+    condition = models.ForeignKey(Condition, on_delete=models.CASCADE, related_name='questionMedia')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='questionMedia')
+
+    img = models.ImageField(blank=True)
+    main_flag = models.BooleanField(default=False)
+
+
+class AnswerMedia(BaseModel):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='answerMedia')
+
+    img = models.ImageField(blank=True)
+    main_flag = models.BooleanField(default=False)
