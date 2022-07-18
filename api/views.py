070a1af9621779523e13c62c6753b0fe72e32d65
gkim <kimkm0419@naver.com>
2022-07-18 13:42:16 +0900

    🔨 #10 Refactor : Create get_object_or_404 function

--- api/views.py
+++ api/views.py
@@ -14,12 +14,18 @@ class QuestionListView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class QuestionDetailView(APIView):
+    def get_object_or_404(self, condition_id, question_id):
+        try:
+            return Question.objects.get(condition=condition_id, pk=question_id)
+        except Question.DoesNotExist:
+            raise Http404
+
     def get(self, request, condition_id, question_id):
-        question = Question.objects.get(condition=condition_id, pk=question_id)
+        question = self.get_object_or_404(condition_id, question_id)
         serializer = QuestionSerializer(question)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, condition_id, question_id):
-        question = Question.objects.get(condition=condition_id, pk=question_id)
+        question = self.get_object_or_404(condition_id, question_id)
         question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
