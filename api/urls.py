f435159588b9709fee8a9a0336ca2bc07ea4a18f
gkim <kimkm0419@naver.com>
2022-07-18 13:32:20 +0900

    ✨ #10 Feat : Create QuestionDetailView and set GET method for it

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('condition/<int:condition_id>/question/', QuestionListView.as_view())
+    path('condition/<int:condition_id>/question/', QuestionListView.as_view()),
+    path('condition/<int:condition_id>/question/<int:question_id>/', QuestionDetailView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -12,3 +12,9 @@ class QuestionListView(APIView):
         questions = Question.objects.filter(condition=condition_id)
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class QuestionDetailView(APIView):
+    def get(self, request, condition_id, question_id):
+        question = Question.objects.get(condition=condition_id, pk=question_id)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data, status=status.HTTP_200_OK)
