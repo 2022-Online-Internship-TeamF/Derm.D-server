09c732063023f88c2bc6012b1056c04040e7b7c1
gkim <kimkm0419@naver.com>
2022-07-26 12:05:34 +0900

    🔨 #30 Refactor : Revise url format

--- api/urls.py
+++ api/urls.py
@@ -5,18 +5,18 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
 
-    path('members/refresh/', TokenRefreshView.as_view()),
+    path('user/refresh', TokenRefreshView.as_view()),
 
-    path('members/register/', RegisterView.as_view()),
-    path('members/login/', LoginView.as_view()),
-    path('members/logout/', LogoutView.as_view()),
+    path('user/register', RegisterView.as_view()),
+    path('user/login', LoginView.as_view()),
+    path('user/logout', LogoutView.as_view()),
 
     path('user', UserDetailView.as_view()),
 
-    path('condition/', ConditionListView.as_view()),
-    path('condition/<int:pk>/', ConditionDetailView.as_view()),
+    path('condition', ConditionListView.as_view()),
+    path('condition/<int:pk>', ConditionDetailView.as_view()),
 
-    path('condition/<int:condition_id>/question/', QuestionListView.as_view()),
-    path('condition/<int:condition_id>/question/<int:question_id>/', QuestionDetailView.as_view()),
+    path('condition/<int:condition_id>/question', QuestionListView.as_view()),
+    path('condition/<int:condition_id>/question/<int:question_id>', QuestionDetailView.as_view()),
 
 ]
